ff8d4c0426b4b85b3bdc70bca6bade6e20d4f1e4
Prakash Raut <prakash_raut@outlook.com>
2025-08-04 12:09:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-//your JS code here. If required.
-const form = document.getElementById('userform');
-const nameInput = document.getElementById('name');
-const ageInput = document.getElementById('age');
+const form = document.getElementById("userform");
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
 
-function checkVotingEligibility(name, age) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (age > 18) {
+    function checkVotingEligibility(name, age) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (age > 18) {
             resolve(`Welcome, ${name}. You can vote.`);
           } else {
             reject(`Oh sorry ${name}. You aren't old enough.`);
           }
-		}, 4000);
-	})
-}
-
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	const name = nameInput.value.trim();
-    const age = Number(ageInput.value);
-
-	if (!name || !age) {
-        alert("Please enter valid details.");
-        return;
+        }, 4000);
+      });
     }
 
-	checkVotingEligibility(name, age)
-        .then((message) => alert(message))
-        .catch((error) => alert(error));
-    });
-})
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
 
+      const name = nameInput.value.trim();
+      const ageValue = ageInput.value.trim();
+
+      // Validation
+      if (name === "" || ageValue === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+
+      const age = Number(ageValue);
+
+      checkVotingEligibility(name, age)
+        .then((msg) => alert(msg))
+        .catch((err) => alert(err));
+    });
