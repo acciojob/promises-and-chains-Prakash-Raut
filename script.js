e42ee042472d12513dd2891105cc0fca13e6a501
Prakash Raut <prakash_raut@outlook.com>
2025-08-04 11:31:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const form = document.getElementById('userform');
+const username = document.getElementById('name');
+const age = document.getElementById('age');
+
+function checkVotingEligibility(name, age) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (age > 18) {
+            resolve(`Welcome, ${name}. You can vote.`);
+          } else {
+            reject(`Oh sorry ${name}. You aren't old enough.`);
+          }
+		}, 4000);
+	})
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const name = nameInput.value.trim();
+    const age = Number(ageInput.value);
+
+	if (!name || !age) {
+        alert("Please enter valid details.");
+        return;
+    }
+
+	checkVotingEligibility(name, age)
+        .then((message) => alert(message))
+        .catch((error) => alert(error));
+    });
+})
+
